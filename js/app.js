4ce6a8af534e90fec0bc247c5521aa8e6b8716ca
sairah814 <sairah814@gmail.com>
2016-02-01 21:22:48 -0500

    Added relative feedback, clicking on new game, and enter to record new guess

--- js/app.js
+++ js/app.js
@@ -1,27 +1,17 @@
 $(document).ready(function () {
 
-    //define computer's guess on page load-- DONE
-    //turn user's guess into int (once they hit Guess!) and compare to computer's guess --DONE
-    //return whether guess is hot or cold (first guess should always be hot?), subsequent guesses compare to user's previous guess
-    //once user guesses, say you won -- DONE
-    //count number of guesses -- DONE
-    //clicking on new game reloads page
-
     /*Declare global variables*/
+
     var answer = parseInt(Math.random() * 100);
     console.log(answer);
     var intGuess;
     var guessCount = 0;
     var oldGuess = 0;
     console.log(intGuess);
-    console.log("the old guess initially is " + oldGuess)
 
     /*Define functions*/
-    //Validation should check it's the right number, that a value has been put in
-    //if all these are met, it should lead to clearing the input
-    //increasing the counter, determining if the user guessed right, and recording their guess
-    function validation(guess) {
 
+    function validation(guess) {
         if (intGuess < 1 || intGuess > 100) {
             $('#feedback').text("Choose a value between 1-100!");
             console.log("This is not a valid entry.");
@@ -36,12 +26,12 @@ $(document).ready(function () {
             counter();
             if (oldGuess != 0) {
                 mainFeedback(answer, oldGuess, intGuess);
+                relativeFeedback(answer, oldGuess, intGuess);
             } else {
                 $('#feedback').text("Nope, guess again!");
             }
             recordGuesses();
         }
-
     }
 
     function recordGuesses() {
@@ -60,37 +50,62 @@ $(document).ready(function () {
 
     function mainFeedback(secret, origguess, newguess) {
         console.log("This is the original old guess " + origguess);
-        var lastDiff = Math.abs(secret - origguess);
-        console.log("The last difference was " + lastDiff);
-        var newDiff = Math.abs(secret - newguess);
-        console.log("The new difference is " + newDiff);
-        if (newguess == secret) {
-            $('#feedback').text("YOU GOT IT!");
-            $('#userGuess').attr("disabled", true);
-        } else if ((newDiff < lastDiff) || (newDiff == lastDiff)) {
-            $('#feedback').text("warmer!");
-
-        } else if (newDiff > lastDiff) {
-            $('#feedback').text("colder");
-
+        var difference = Math.abs(secret - newguess);
+        if (difference >= 50) {
+            $('#feedback').text("Ice Cold!");
+        } else if (difference <= 49 && difference >= 30) {
+            $('#feedback').text("Cold!");
+        } else if (difference <= 29 && difference >= 20) {
+            $('#feedback').text("Warm");
+        } else if (difference <= 20 && difference >= 10) {
+            $('#feedback').text("Hot!");
+        } else if (difference <= 9 && difference >= 1) {
+            $('#feedback').text("Very Hot!!");
         } else {
-            console.log("error in comparing");
+            $('#feedback').text("YOU WON!");
+            $('#userGuess').attr("disabled", true);;
         }
     }
 
-    function relativeFeedback() {
+    function relativeFeedback(secret, origguess, newguess) {
+        var lastDiff = Math.abs(secret - origguess);
+        var newDiff = Math.abs(secret - newguess);
+        if (secret == newguess) {
+            $('#relative-feedback').text("WOOOHOOO!");
+            $('#userGuess').attr("placeholder", "You Got It!");
+        } else if (newDiff < lastDiff) {
+            $('#relative-feedback').text("You're warmer than your last guess.");
 
+        } else if (newDiff > lastDiff) {
+            $('#relative-feedback').text("You're colder than your last guess.");
+
+        } else { // assumes newDiff == lastDiff
+            $('#relative-feedback').text("Your guess is just as far as the last one.");
+        }
     }
 
     /*Call functions (triggered on button click)*/
+
     $('#guessButton').click(function () {
         intGuess = parseInt($('#userGuess').val(), 10);
         console.log("Your guess is " + intGuess);
         validation(intGuess);
         oldGuess = intGuess;
-        console.log("This is the new old guess " + oldGuess);
     });
 
+    $('#userGuess').on('keypress', function (event) {
+        if (event.which === 13) {
+            event.preventDefault();
+            intGuess = parseInt($('#userGuess').val(), 10);
+            console.log("Your guess is " + intGuess);
+            validation(intGuess);
+            oldGuess = intGuess;
+        }
+    });
+
+    $('.new').click(function () {
+        newGame();
+    });
 
     /*--- Display information modal box ---*/
     $(".what").click(function () {
